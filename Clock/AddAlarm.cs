0801ee68de72e878a42a2ebe22c1a268706b2f81
Igor4358 <igor4358@yandex.ru>
2024-10-29 19:53:14 +0300

    Add files via upload

---
+++ Clock/AddAlarm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class AddAlarm : Form
+    {
+       public Alarm Alarm { get; set; }
+        public AddAlarm()
+        {
+            InitializeComponent();
+            Alarm = new Alarm();
+        }
+        void InitAlarm()
+        {
+            Alarm.Date = dateTimePickerDate.Value;
+            Alarm.Time = dateTimePickerTime.Value;
+            Alarm.Filename = labelFilename.Text;
+            for (int i = 0; i < checkedListBoxWeek.Items.Count; i++)
+            {
+                Alarm.Weekdays[i] = (checkedListBoxWeek.Items[i] as CheckBox).Checked;
+            }
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            InitAlarm();
+            this.Close();
+        }
+
+        private void checkBoxExactDate_CheckedChanged(object sender, EventArgs e)
+        {
+            dateTimePickerDate.Enabled = ((CheckBox)sender).Checked;
+        }
+    }
+}
---
+++ Clock/MainForm.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace Clock
+{
+
+    public partial class MainForm : Form
+    {
+        ColorDialog backgroundColorDialog;
+        ColorDialog foregroundColorDialog;
+        ChooseFont chooseFontDialog;
+        AlarmList alarmsList;
+        string FontFile { get;set; }
+      
+        public MainForm()
+        {
+            InitializeComponent();
+            SetFontDirectory();
+            this.TransparencyKey = Color.Empty;
+            backgroundColorDialog = new ColorDialog();
+            foregroundColorDialog = new ColorDialog();
+         
+            chooseFontDialog = new ChooseFont();
+            LoadSettings();
+
+            alarmsList = new AlarmList();
+            //  backgroundColorDialog.Color = Color.Black;
+            // foregroundColorDialog.Color = Color.Blue;
+
+            SetVisibility(false);
+            this.Location = new Point
+                (
+                    System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width - this.Width,
+                    50
+                );
+            this.Text += $" Location: {this.Location.X}x{this.Location.Y}";
+        
+            
+        }
+        void SetFontDirectory()
+        {
+            string location = Assembly.GetEntryAssembly().Location;
+            string path = Path.GetDirectoryName(location);
+          //  MessageBox.Show(path);
+
+            Directory.SetCurrentDirectory($"{path}\\..\\..\\Fonts");
+            //MessageBox.Show(Directory.GetCurrentDirectory());
+        }
+        void LoadSettings()
+        {
+            StreamReader sr = new StreamReader("settings.txt");
+            List<string> settings = new List<string>();
+            while (!sr.EndOfStream)
+            {
+                settings.Add(sr.ReadLine());
+            }
+            sr.Close();
+            backgroundColorDialog.Color = Color.FromArgb(Convert.ToInt32(settings.ToArray()[0]));
+            foregroundColorDialog.Color = Color.FromArgb(Convert.ToInt32(settings.ToArray()[1]));
+            FontFile = settings.ToArray()[2];
+            topmostToolStripMenuItem.Checked = bool.Parse(settings.ToArray()[3]);
+            showDateToolStripMenuItem.Checked = bool.Parse(settings.ToArray()[4]);
+            labelTime.Font = chooseFontDialog.SetFontFile(FontFile);
+            labelTime.ForeColor = foregroundColorDialog.Color;
+            labelTime.BackColor = backgroundColorDialog.Color;
+
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",true);
+            object run = rk.GetValue("Clock318");
+            if (run != null) loadOnWindowsStartupToolStripMenuItem.Checked = true;
+
+            rk.Dispose();
+        }
+        void SaveSettings()
+        {
+            StreamWriter sw = new StreamWriter("settings.txt");
+            sw.WriteLine(backgroundColorDialog.Color.ToArgb());
+            sw.WriteLine(foregroundColorDialog.Color.ToArgb());
+            sw.WriteLine(chooseFontDialog.FontFile.Split('\\').Last());
+            sw.WriteLine(topmostToolStripMenuItem.Checked);
+            sw.WriteLine(showDateToolStripMenuItem.Checked);
+            sw.Close();
+            Process.Start("notepad", "settings.txt");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToString("HH.mm.ss ");
+            if(cbShowDate.Checked)
+            {
+                labelTime.Text += $"\n{DateTime.Today.ToString("yyyy.MM.dd")}";
+            }
+            notifyIconSystemTray.Text = "Current time" + labelTime.Text;
+        }
+        private void SetVisibility(bool visible)
+        {
+            this.TransparencyKey = visible ? Color.Empty:this.BackColor;
+            this.FormBorderStyle = visible ? FormBorderStyle.Sizable:FormBorderStyle.None;
+            this.ShowInTaskbar = visible;
+            cbShowDate.Visible = visible;
+            btnHideControls.Visible = visible;
+            labelTime.BackColor = visible?Color.Empty:Color.Coral;
+        }
+        private void bntHideControls_Click(object sender, EventArgs e)
+        {
+            //SetVisibility(false);
+            showControlsToolStripMenuItem.Checked = false;
+            notifyIconSystemTray.ShowBalloonTip(3,"Alerts! ","чтобы вернуть как было нажать 2 раза", ToolTipIcon.Info);
+        }
+
+        private void labelTime_DoubleClick(object sender, EventArgs e)
+        {
+            SetVisibility(true);
+        }
+
+       // private void notifyIconSystemTray_BalloonTipShown(object sender, EventArgs e)
+        //{
+          //  notifyIconSystemTray.Text = "Current time" + labelTime.Text;
+       // }
+
+        private void notifyIconSystemTray_MouseMove(object sender, MouseEventArgs e)
+        {
+            notifyIconSystemTray.Text = "Current time\n" + labelTime.Text;
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void topmostToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = topmostToolStripMenuItem.Checked;
+        }
+
+        private void showControlsToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            SetVisibility(((ToolStripMenuItem)sender).Checked);
+        }
+
+        private void showDateToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            showDateToolStripMenuItem.Checked = ((CheckBox)sender).Checked;
+        }
+
+        private void notifyIconSystemTray_DoubleClick(object sender, EventArgs e)
+        {
+            if(!this.TopMost)
+            {
+                this.TopMost = true;
+                this.TopMost = false;
+            }
+        }
+
+        private void foregroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(foregroundColorDialog.ShowDialog(this)==DialogResult.OK)
+            {
+                labelTime.ForeColor = foregroundColorDialog.Color;
+            }
+        }
+
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(backgroundColorDialog.ShowDialog(this)==DialogResult.OK)
+            {
+                labelTime.BackColor = backgroundColorDialog.Color;
+            }
+        }
+        //private void
+        
+
+        private void fontsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(chooseFontDialog.ShowDialog(this)==DialogResult.OK)
+            {
+                labelTime.Font = chooseFontDialog.ChoosenFont;
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveSettings();
+        }
+
+        private void loadOnWindowsStartupToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",true);
+            if (loadOnWindowsStartupToolStripMenuItem.Checked)
+                rk.SetValue("Clock318", Application.ExecutablePath);
+            else rk.DeleteValue("Clock318", false);
+            rk.Dispose();
+        }
+
+        private void alarmsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            alarmsList.ShowDialog(this);
+        }
+    }
+}
